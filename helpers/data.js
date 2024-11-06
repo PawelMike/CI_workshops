dd50730e1feceb47fd9b54ca92253c44b1b68e87
PawelMike <michalkowski.pawel@gmail.com>
2024-11-06 14:05:12 +0100

    refactor + generate token test

--- helpers/data.js
+++ helpers/data.js
@@ -1,2 +1,4 @@
 export const userID = "680d40ae-540a-4766-a846-fdd8687fe9e1"
 export const baseURL = "https://demoqa.com"
+export const user = "test_warez_pmqa"
+export const password = process.env.SECRET_PASSWORD
--- tests/api.test.js
+++ tests/api.test.js
@@ -2,24 +2,45 @@ import { expect } from "chai";
 import pkg from "pactum";
 const { spec } = pkg;
 import "dotenv/config";
-import { baseURL, userID } from "../helpers/data.js"
+import { baseURL, userID, user, password } from "../helpers/data.js"
+
+let token_response;
 
 describe("API tests", () => {
   it("get request", async () => {
     const response = await spec().get(`${baseURL}/BookStore/v1/Books1`)
+    .inspect();
     const responseB = JSON.stringify(response.body);
     expect(response.statusCode).to.eql(200);
     // expect(responseB).to.include("Git Pocket Guide");
   });
 
-  it("Crate a user", async () => {
+  it.skip("Crate a user", async () => {
     const response = await spec()
       .post(`${baseURL}/Account/v1/User`)
       .withBody({
-        userName: "test_warez_pmqa",
-        password: process.env.SECRET_PASSWORD,
+        userName: user,
+        password: password,
       })
       .inspect();
     expect(response.statusCode).to.eql(201);
   });
+
+  it("Generate User Token", async () => {
+    const response = await spec()
+      .post(`${baseURL}/Account/v1/GenerateToken`)
+      .withBody({
+        userName: user,
+        password: password,
+      })
+      .inspect();
+      token_response = response.body.token;
+      console.log(token_response);
+    expect(response.statusCode).to.eql(200);
+    expect(response.body.result).to.eql("User authorized successfully.");
+  });
+
+  it.only("Check token", async () => {
+    console.log("another it block " + token_response)
+  })
 });
